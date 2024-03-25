45a07187a556444390e933861ee3c1a636289655
AdrianoVM <74271524+AdrianoVM@users.noreply.github.com>
2024-03-25 10:53:52 +0100

    bug: fixed publication text

--- personal-website/src/components/publications/publications.js
+++ personal-website/src/components/publications/publications.js
@@ -11,7 +11,6 @@ function Publications() {
         <Accordion.Header><b><FontAwesomeIcon icon={faVrCardboard}/> Cybersickness Assessment Framework <FontAwesomeIcon icon={faToolbox}/></b></Accordion.Header>
         <Accordion.Body>
           Published in the 2023 IEEE Conference on Virtual Reality and 3D User Interfaces. Available <a href='https://ieeexplore.ieee.org/abstract/document/10108853'>here</a>.<br/>
-          <FontAwesomeIcon icon={faStar}/> Best demo/poster award at the conference !
           More details in <HashLink to="/projects#csaf">project page <FontAwesomeIcon icon={faArrowAltCircleRight}/></HashLink><br/><br/>
           <b>Abstract:</b><br/>
           Studies of cybersickness (CS) often require a significant investment in creating the primary VR environment and other experiment-related features.
@@ -24,7 +23,9 @@ function Publications() {
       <Accordion.Item eventKey="1">
         <Accordion.Header><b><FontAwesomeIcon icon={faVrCardboard}/> Forest SaVR <FontAwesomeIcon icon={faTree}/></b></Accordion.Header>
         <Accordion.Body>
-        Published in the 2020 GI VR / AR Workshop, Gesellschaft für Informatik. Available <a href='https://dl.gi.de/server/api/core/bitstreams/43625e2e-995c-439f-a85b-ed29fffed164/content'>here</a>. More details in <HashLink to="/projects#forest-savr">project page <FontAwesomeIcon icon={faArrowAltCircleRight}/></HashLink><br/><br/>
+        Published in the 2020 GI VR / AR Workshop, Gesellschaft für Informatik. Available <a href='https://dl.gi.de/server/api/core/bitstreams/43625e2e-995c-439f-a85b-ed29fffed164/content'>here</a>. <br/>
+        <FontAwesomeIcon icon={faStar}/> Best demo/poster award at the conference !
+        More details in <HashLink to="/projects#forest-savr">project page <FontAwesomeIcon icon={faArrowAltCircleRight}/></HashLink><br/><br/>
         <b>Abstract:</b><br/>
         Deforestation is a serious issue affecting climate and contributing to global warming.
         This demonstration presents Forest SaVR, an interactive virtual-reality application where users can explore a realistic forest environment,
